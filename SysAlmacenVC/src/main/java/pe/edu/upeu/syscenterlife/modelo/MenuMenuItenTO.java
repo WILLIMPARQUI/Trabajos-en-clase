67190ef8759b46c63e0647557914dc1554590199
WILLIMPARQUI <willimneymar72@gmail.com>
2024-05-22 22:39:51 -0500

    trabajo 20/05/24

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/MenuMenuItenTO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MenuMenuItenTO {
+
+    public String menunombre, menuitemnombre, nombreObj;
+
+}
